dd22224c9fae20b59179bf6541693a8fd49acb98
jorge90125 <jorge.gonzalez7@g.austincc.edu>
2022-11-19 09:49:25 -0600

    App runs and login route working

--- stax-on-stax-frontend/src/App.js
+++ stax-on-stax-frontend/src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react'
+import {Route, Routes, useNavigate} from 'react-router-dom'
+import SignIn from './components/SignIn'
 
-function App() {
+let baseURL = 'http://localhost:8000'
+
+const App = () => {
+
+  const login = async (e) => {
+    console.log(e.target.email.value)
+    e.preventDefault()
+    const loginForm = {
+      email: e.target.email.value,
+      password: e.target.password.value
+    }
+    try {
+      const res = await fetch(`${baseURL}/users/login`, {
+        method: 'POST',
+        body: JSON.stringify(loginForm),
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        credentials: 'include'
+      })
+      if (res.status === 200) {
+        console.log(res.status)
+      }
+    }
+    catch (err) {
+      console.log(err)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path='/' element={<SignIn login={login}/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ stax-on-stax-frontend/src/components/SignIn.js
@@ -0,0 +1,16 @@
+const SignIn = (props) => {
+    return(
+        <div>
+            <h1>Welcome! Please sign-in or click the register tab to register.</h1>
+            <form onSubmit={props.login}>
+                <label htmlFor='name'>Email: </label>
+                <input type='text' id='email' name='email' />
+                <label htmlFor='name'>Password: </label>
+                <input type='text' id='password' name='password' />
+                <input type='submit' value='Log In' />
+            </form>
+        </div>
+    )
+}
+
+export default SignIn
