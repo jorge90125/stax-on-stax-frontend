8ca51f1bd1c59c2b7cfc19bb2a76b725b657ff3e
jorge90125 <jorge.gonzalez7@g.austincc.edu>
2022-11-25 13:48:18 -0600

    Can sort collection descending by field

--- stax-on-stax-frontend/src/components/RecordContainer.js
+++ stax-on-stax-frontend/src/components/RecordContainer.js
@@ -1,16 +1,32 @@
+import React, {useState} from "react"
+
 const RecordContainer = (props) => {
+    let sortedRecords = props.records
+
+    const [sortedField, setSortedField] = useState('artist')
+
+    sortedRecords.sort((a, b) => {
+        if (a[sortedField] < b[sortedField]) {
+            return -1
+        }
+        if (a[sortedField] > b[sortedField]) {
+            return 1
+        }
+        return 0
+    })
+
     return(
         <div>
             <h1>This is the record container.</h1>
             <table>
                 <tr>
                     <th>Artwork</th>
-                    <th>Artist</th>
-                    <th>Album</th>
-                    <th>Year</th>
-                    <th>Genre</th>
+                    <th>Artist<button onClick={() => setSortedField('artist')}>^</button></th>
+                    <th>Album<button onClick={() => setSortedField('name')}>^</button></th>
+                    <th>Year<button onClick={() => setSortedField('release_year')}>^</button></th>
+                    <th>Genre<button onClick={() => setSortedField('genre')}>^</button></th>
                 </tr>
-                {props.records.map((record) => {
+                {sortedRecords.map((record) => {
                     return(
                         <tr onClick={() => props.showRecord(record.id)}>
                             <td>Art Here</td>
