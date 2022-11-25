6ed009eff8dfdc9fa7a444ad5f60b56c16361a45
jorge90125 <jorge.gonzalez7@g.austincc.edu>
2022-11-25 13:27:52 -0600

    Switched record collection to table

--- stax-on-stax-frontend/src/components/RecordContainer.js
+++ stax-on-stax-frontend/src/components/RecordContainer.js
@@ -2,15 +2,26 @@ const RecordContainer = (props) => {
     return(
         <div>
             <h1>This is the record container.</h1>
-            {props.records.map((record) => {
-                return(
-                    <div onClick={() => props.showRecord(record.id)}>
-                        <h2>{record.artist}</h2>
-                        <h2>{record.name}</h2>
-                        <h2>{record.release_year}</h2>
-                    </div>
-                )
-            })}
+            <table>
+                <tr>
+                    <th>Artwork</th>
+                    <th>Artist</th>
+                    <th>Album</th>
+                    <th>Year</th>
+                    <th>Genre</th>
+                </tr>
+                {props.records.map((record) => {
+                    return(
+                        <tr onClick={() => props.showRecord(record.id)}>
+                            <td>Art Here</td>
+                            <td>{record.artist}</td>
+                            <td>{record.name}</td>
+                            <td>{record.release_year}</td>
+                            <td>{record.genre}</td>
+                        </tr>
+                    )
+                })}
+            </table>
         </div>
     )
 }
